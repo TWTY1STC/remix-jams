36f5c1755bdd7b551ed6e7cdb3c51fa0374abc44
TWTY1STC <liza.njuguna@gmail.com>
2016-08-03 16:33:13 +0000

    refactor reveal points animation

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,27 +1,14 @@
 var animatePoints = function(){
     var points = document.getElementsByClassName('point');
     
-    var revealFirstPoint = function(){
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scale(1) translateY(0)";
-        points[0].style.msTransform = "scale(1) translateY(0)";
-        points[0].style.WebkitTransform = "scale(1) translateY(0)";
+    var revealPoint = function(index) {
+            points[index].style.opactiy = 1;
+            points[index].style.transform = "scale(1) translateY(0)";
+            points[index].style.msTransform = "scale(1) translateY(0)";
+            points[index].style.WebkitTransform = "scale(1) translateY(0)";
     };
     
-    var revealSecondPoint = function(){
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scale(1) translateY(0)";
-        points[1].style.msTransform = "scale(1) translateY(0)";
-        points[1].style.WebkitTransform = "scale(1) translateY(0)";
-    };
-    
-    var revealThirdPoint = function(){
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scale(1) translateY(0)";
-        points[2].style.msTransform = "scale(1) translateY(0)";
-        points[2].style.WebkitTransform = "scale(1) translateY(0)";
-    };
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
+    for (var i =0; i< points.length; i++){
+        revealPoint(i);
+    }
 };
