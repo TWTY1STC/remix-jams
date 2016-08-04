c90127910966016ec448337b12b76bfc542ef0eb
TWTY1STC <liza.njuguna@gmail.com>
2016-08-04 13:30:03 +0000

    add scroll event listener to selling points

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,29 @@
 var points = document.getElementsByClassName('point');
 
 var animatePoints = function(points){
-    var revealPoint = function(points) {
-        for(var point in points){
-            points[point].style.opactiy = 1;
-            points[point].style.transform = "scale(1) translateY(0)";
-            points[point].style.msTransform = "scale(1) translateY(0)";
-            points[point].style.WebkitTransform = "scale(1) translateY(0)";
+    var revealPoint = function(index) {
+            points[index].style.opacity = 1;
+            points[index].style.transform = "scale(1) translateY(0)";
+            points[index].style.msTransform = "scale(1) translateY(0)";
+            points[index].style.WebkitTransform = "scale(1) translateY(0)";
+    };
+    
+    for (var i =0; i< points.length; i++){
+        revealPoint(i);
+    }
+};
+
+window.onload = function(){
+    if(window.innerHeight > 950){
+        animatePoints(points);
     }
     
-    revealPoint(points);
-    };
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event){
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(points);
+        }
+    });
 };
