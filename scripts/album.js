5e903ee591063589f119a20b6e0ef265b50ce620
TWTY1STC <liza.njuguna@gmail.com>
2016-08-09 00:45:05 +0000

    edit findParent function

--- scripts/album.js
+++ scripts/album.js
@@ -62,7 +62,7 @@ var setCurrentAlbum = function(album) {
 var findParentByClassName = function(element, targetClass){
     if(element){
         var currentParent = element.parentElement
-        while(currentParent != targetClass && currentParent.className !== null) {
+        while(currentParent.className != targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
         return currentParent;
